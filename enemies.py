8e9525027e71c97ee449f2c1c748affa7887efc9
Полина <>
2016-11-17 16:53:13 +0300

    Changed Enemies + Trolls

--- enemies.py
+++ enemies.py
@@ -39,8 +39,80 @@ class GreenDragon(Dragon):
         self.set_answer(x + y)
         return self.__quest
 
-#FIXME здесь также должны быть описаны классы RedDragon и BlackDragon
+
+class RedDragon(Dragon):
+    def __init__(self):
+        self._health = 200
+        self._attack = 10
+        self._color = 'красный'
+    def question(self):
+        x = randint(1,100)
+        y = randint(1,100)
+        self.__quest = str(x) + '-' + str(y)
+        self.set_answer(x - y)
+        return self.__quest
+
+class BlackDragon(Dragon):
+    def __init__(self):
+        self._health = 200
+        self._attack = 10
+        self._color = 'чёрный'
+
+    def question(self):
+        x = randint(1, 100)
+        y = randint(1, 100)
+        self.__quest = str(x) + '*' + str(y)
+        self.set_answer(x*y)
+        return self.__quest
+
+
 # красный дракон учит вычитанию, а чёрный -- умножению.
 
 
-enemy_types = [GreenDragon, RedDragon, BlackDragon]
+class Troll(Enemy):
+    def set_answer(self, answer):
+        self.__answer = answer
+
+    def check_answer(self, answer):
+        return answer == self.__answer
+
+class GuessTroll(Troll):
+    def __init__(self):
+        self._health = 200
+        self._attack = 10
+        self._color = 'угадай -'
+
+    def question(self):
+        x = randint(1, 5)
+        self.__quest = 'Угадай число от 1 до 5'
+        self.set_answer(x)
+        return self.__quest
+
+def Simple(x):
+    i = 2
+    j = 0
+    while i**2 <= x and j != 1:
+        if x%i == 0:
+            j = 1
+        i += 1
+    if j == 1:
+        return False
+    else:
+        return True
+
+class SimpleTroll(Troll):
+    def __init__(self):
+        self._health = 200
+        self._attack = 10
+        self._color = 'просто -'
+
+    def question(self):
+        x = randint(1, 100)
+        self.__quest = 'Простое ли число ' + str(x) + '? Да - 1, нет - 0'
+        if Simple(x) == True:
+            self.set_answer(1)
+        else:
+            self.set_answer(0)
+        return self.__quest
+
+enemy_types = [GreenDragon, RedDragon, BlackDragon, SimpleTroll, GuessTroll]
